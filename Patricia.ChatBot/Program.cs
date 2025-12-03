a1ec0d53baf4eefa786d723bb6cc60108973e820
Johnny Anderson Nascimento <nascimentoho1998@gmail.com>
2025-12-02 22:31:21 -0300

    fix: Ajuste nas rotas dos endpoints.

--- Patricia.ChatBot/Program.cs
+++ Patricia.ChatBot/Program.cs
@@ -1,4 +1,8 @@
 
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+using Patricia.ChatBot.Services;
+
 namespace Patricia.ChatBot
 {
     public class Program
@@ -14,19 +18,36 @@ namespace Patricia.ChatBot
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
+            builder.Services.AddSingleton<GeminiService>();
+            builder.Services.AddSingleton<DatasetService>();
+
+            builder.Services.AddDbContext<AppDbContext>(opt =>
+                opt.UseInMemoryDatabase("PatriciaChatBotDB"));
+
+            builder.Services.AddScoped<UserService>();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
+                app.UseSwagger(c =>
+                {
+                    c.PreSerializeFilters.Add((swagger, httpReq) =>
+                    {
+                        swagger.Servers = new List<OpenApiServer>
+                        {
+                            new OpenApiServer { Url = $"{httpReq.Scheme}://{httpReq.Host.Value}/api/csharp" }
+                        };
+                    });
+                });
                 app.UseSwaggerUI();
             }
 
+            app.UsePathBase("/api/csharp");
             app.UseHttpsRedirection();
-
+            app.UseRouting();
             app.UseAuthorization();
-
 
             app.MapControllers();
 
